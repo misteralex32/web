86852872578f83e68d9f2de157985d7a4a1f4428
7777722222 <mister.darovskih@yandex.ru>
2022-10-04 17:53:40 +0300

    fix

--- app/server.py
+++ app/server.py
@@ -4,10 +4,18 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+health_status = True
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+
+@app.route('/toggle')
+def toggle():
+    global health_status
+    health_status = not health_status
+    return jsonify(health_value=health_status)
 
 @app.route('/health')
 def health():
