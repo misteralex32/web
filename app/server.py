bf1dd64267e36d2a7ba83e23420cb7b6635e93c1
7777722222 <mister.darovskih@yandex.ru>
2022-10-04 15:12:16 +0300

    add

--- app/server.py
+++ app/server.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
+def main():
+    return 'OK'
 def hello_world():
     return 'Hello World!'
 
