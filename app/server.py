6b0beb0877def019d3d8c43a3e6ea4178a8d57a9
7777722222 <mister.darovskih@yandex.ru>
2022-10-04 17:07:43 +0300

    fix

--- app/server.py
+++ app/server.py
@@ -9,9 +9,16 @@ metrics = PrometheusMetrics(app)
 def hello_world():
     return 'Hello World!'
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    return
+    if health_status:
+        resp = jsonify(health="healthy")
+        resp.status_code = 200
+    else:
+        resp = jsonify(health="unhealthy")
+        resp.status_code = 500
+
+    return resp
     
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run()
