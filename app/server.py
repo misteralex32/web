98b75825070da5dc775f97ff7a1bf15415356e6b
7777722222 <mister.darovskih@yandex.ru>
2022-10-04 15:24:01 +0300

    add

--- app/server.py
+++ app/server.py
@@ -1,13 +1,48 @@
+import time
+import random
+
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
-app = Flask(__name__)
-metrics = PrometheusMetrics(app)
 
-@app.route('/')
-def main():
-    return 'OK'
+
+app = Flask(__name__)
+PrometheusMetrics(app)
+
+
+endpoints = ("one", "two", "three", "four", "five", "error")
+
+
+@app.route("/one")
 def hello_world():
     return 'Hello World!'
 
+def first_route():
+    time.sleep(random.random() * 0.2)
+    return "ok"
+
+
+@app.route("/two")
+def the_second():
+    time.sleep(random.random() * 0.4)
+    return "ok"
+
+
+@app.route("/three")
+def test_3rd():
+    time.sleep(random.random() * 0.6)
+    return "ok"
+
+
+@app.route("/four")
+def fourth_one():
+    time.sleep(random.random() * 0.8)
+    return "ok"
+
+
+@app.route("/error")
+def oops():
+    return ":(", 500
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run("0.0.0.0", 5000, threaded=True)
